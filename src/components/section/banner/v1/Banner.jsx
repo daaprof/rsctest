7eb97368e560cd2313c02ca30d8605b4b7d4a70f
KiterWork <107297392+KiterWork@users.noreply.github.com>
2022-08-12 12:15:25 +0200

    Update Banner.jsx
    Removed shitty yellow thing Mint is live

--- src/components/section/banner/v1/Banner.jsx
+++ src/components/section/banner/v1/Banner.jsx
@@ -2,10 +2,7 @@ import { useModal } from "../../../../utils/ModalContext";
 import Counter from "../../../../common/counter";
 import Button from "../../../../common/button";
 import BannerV1Wrapper from "./Banner.style";
-
 import characterThumb from "../../../../assets/images/nft/Character1.png";
-import mintLiveDownArrow from "../../../../assets/images/nft/mint_live_down_arrow.svg";
-import mintLiveText from "../../../../assets/images/nft/mint_live_text.png";
 import homeImageBG from "../../../../assets/images/nft/home_img_bg.png";
 import homeInfo from "../../../../assets/data/bannerV1.json";
 
@@ -22,14 +19,14 @@ const Banner = () => {
                 <span className="count">
                   <Counter end={3000} duration={3000} />
                 </span>{" "}
-                / 3001 Minted
+                / 3000 Minted
               </h3>
               <div className="banner_buttons">
                 <Button lg variant="mint" onClick={() => mintModalHandle()}>
                   {" "}
                   Mint now
                 </Button>
-                <Button lg variant="outline" onClick={() => window.open('https://discord.gg/BC3SdW3cfG', 'blank')}>
+                <Button lg variant="outline" onClick={() => {}}>
                   Wishlist now
                 </Button>
               </div>
@@ -43,16 +40,7 @@ const Banner = () => {
           <div className="col-lg-6">
             <div className="rsc_v1_baner_right">
               <div className="rsc_v1_baner_right_img_sect">
-                <div className="mint_live_circle_sect">
-                  <div className="mint_live_circle">
-                    <span>
-                      <img src={mintLiveDownArrow} alt="" />
-                    </span>
-                    <span className="mint_live_text rotated-style">
-                      <img src={mintLiveText} alt="" />
-                    </span>
-                  </div>
-                </div>
+
                 <div className="rsc_v1_baner_right_img_bg">
                   <img src={homeImageBG} alt="" />
                 </div>
