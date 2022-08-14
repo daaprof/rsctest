a5be85fb688f5abd28016cbd42ae83891d8cce96
KiterWork <107297392+KiterWork@users.noreply.github.com>
2022-08-14 21:08:19 +0200

    Update roadMapV1.js

--- src/assets/data/roadMap/roadMapV1.js
+++ src/assets/data/roadMap/roadMapV1.js
@@ -1,45 +1,45 @@
 import checkIcon from "../../images/icon/roadmap_check.svg"
-import spinnerIcon from "../../images/icon/roadmap_spiner.png"
+import emptyImage from "../../images/icon/emptyImage.png"
 
 const data = [
     {
-        icon: spinnerIcon,
+        icon: emptyImage,
         title: "Phase 01 ",
         subtitle: "10% minted",
         text: "We will list the soldiers depending on the percentage of rarity they have. This percentage will influence the amount of exclusivities its holder can participate in."
     }, 
     {
-        icon: spinnerIcon,
+        icon: emptyImage,
         title: "Phase 02",
         subtitle: "20% minted",
         text: "We will release the benefits of each group and announce the use of our NFTs as their holder’s digital passport."
     }, 
     {
-        icon: spinnerIcon,
+        icon: emptyImage,
         title: "Phase 03",
         subtitle: "30% minted",
         text: "We will start our monthly giveaway of trips to the holders of Soldiers, this trip will be all expenses paid by RSC, as well as hosting parties in Spain and across Europe."
     }, 
     {
-        icon: spinnerIcon,
+        icon: emptyImage,
         title: "Phase 04",
         subtitle: "50% minted",
         text: "We will apply to get verified on OpenSea!"
     }, 
     {
-        icon: spinnerIcon,
+        icon: emptyImage,
         title: "Phase 05",
         subtitle: "70% minted",
         text: "We will open the RSC offices in Dubai, where the team will be developing RSC V2 together and continue the journey with RSC."
     }, 
     {
-        icon: spinnerIcon,
+        icon: emptyImage,
         title: "Phase 06",
         subtitle: "80% minted",
         text: "RSC V2 will be ready to deploy and waiting for 100% of the first Soldiers to be sold. The rarity of RSC Soldiers will influence the features its holder can access in V2, to improve the utilization of Soldiers."
     }, 
     {
-        icon: spinnerIcon,
+        icon: emptyImage,
         title: "Phase 07",
         subtitle: "100% SOLD OUT",
         text: "We are only getting started. We will be able to dedicate ourselves full time to deploying and growing the Recon Soldiers Club, providing as many of our community with monthly perks as possible."
